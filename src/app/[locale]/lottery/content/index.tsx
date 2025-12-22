62d5f047c2e94198dba6145aec54b177c218dee7
wangmr <wangmr0323@gmail.com>
2025-12-23 06:06:12 +0800

    # 调整边距

--- src/app/[locale]/lottery/content/index.tsx
+++ src/app/[locale]/lottery/content/index.tsx
@@ -109,7 +109,7 @@ const LotteryContent: React.FC = () => {
   };
 
   return (
-    <section className="relative py-20 md:py-32">
+    <section className="relative py-12 md:py-24">
       {/* Background Elements */}
       <div className="absolute inset-0 z-0">
         {/* Animated Background Shapes */}
