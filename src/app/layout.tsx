a3ddaba6709209a74dd8203b07ace608200d329b
DaiYz <dyz2924ever@gmail.com>
2026-01-10 16:10:05 +0800

    feat:   添加 <SpeedInsights />

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,6 +36,7 @@ export default async function RootLayout({ children }: Props) {
             <Providers>
               {children}
               <Analytics />
+              <SpeedInsights />
               </Providers>
           </NextIntlClientProvider>
         </div>
